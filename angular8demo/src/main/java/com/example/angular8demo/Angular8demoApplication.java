ed429e8203ad8fded96c781c4e10b69402d0269f
pooja <poojapandit1258@gmail.com>
2022-06-10 17:07:33 +0530

    angular8demo

---
+++ angular8demo/src/main/java/com/example/angular8demo/Angular8demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.angular8demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular8demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angular8demoApplication.class, args);
+	}
+
+}
---
+++ angular8demo/src/test/java/com/example/angular8demo/Angular8demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.angular8demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Angular8demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
